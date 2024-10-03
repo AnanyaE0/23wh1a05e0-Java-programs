fe050e942f47451844dfa098fcf6d9f94283c304
AnanyaE0 <154441956+AnanyaE0@users.noreply.github.com>
2024-10-03 12:17:06 +0530

    Add files via upload

---
+++ decrement.java
@@ -0,0 +1,7 @@
+class decrement{
+	public static void main(String[] args){
+		int a = 6;
+		int b = --a;
+		System.out.println("decrement:"+ b); 
+}
+}
---
+++ elif.java
@@ -0,0 +1,9 @@
+class elif{
+	public static void main(String[] args){
+		int a = 12;
+		if(a%2==0){
+		 System.out.print("Even");}
+		else
+			System.out.print("Odd");
+	}
+}
