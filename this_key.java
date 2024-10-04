59b6a9c40e531fb68483cb729311725d7f2464ea
AnanyaE0 <154441956+AnanyaE0@users.noreply.github.com>
2024-10-04 13:39:32 +0530

    Add files via upload

---
+++ this_key.java
@@ -0,0 +1,18 @@
+package csed8;
+
+
+public class this_key {
+void name() {
+	this.age();
+	System.out.println("name");} 
+void age() {
+	System.out.println("age");
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		this_key x = new this_key();
+		x.name();
+	}
+
+}
